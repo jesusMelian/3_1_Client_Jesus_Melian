44cb1ed7f6df6a0862e7dac66dd26f93c7b1677d
jesusMelian <78150247+jesusMelian@users.noreply.github.com>
2022-02-01 12:51:41 +0000

    Features:  implement goodbye for unlogin

--- src/com/jesusmelian/Main.java
+++ src/com/jesusmelian/Main.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
+
 
 public class Main {
 
@@ -16,6 +21,12 @@ public class Main {
         boolean seguir = true;
         boolean first = true;
         Scanner sc = new Scanner(System.in);
+        String[] listMsg = null;
+        String user = null;
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        String returnValue = null;
+        System.out.println("Hora actual: " + dateFormat.format(date));
 
         Socket socket = new Socket("localhost", PORT);
 
@@ -27,24 +38,40 @@ public class Main {
             try {
                 if(first) {
                     System.out.println("INTRODUZCA EL NOMBRE DE USUARIO: ");
-                    String user = sc.nextLine();
-                    oos.writeObject(user);
-                    first = false;
+                    user = sc.nextLine();
+                    if (user != null) {
+                        oos.writeObject(user);
+                        first = false;
+                        listMsg = (String[]) ois.readObject();
+                    }
                 }else{
-                    System.out.println("INTRODUZCA EL MENSAJE: debe comenzar con'message:'");
-                }
+                    if(listMsg != null){
+                        System.out.println("LISTA DE MENSAJES: ");
+                        for(String msgs: listMsg){
+                            System.out.println(msgs);
+                        }
+                        if(user != null) {
+                            System.out.println("INTRODUZCA EL MENSAJE: debe comenzar con'message:'");
+                            String msg = sc.nextLine();
+                            oos.writeObject(msg);
+                            //Leo lo que obtengo del servidor
+                            returnValue = (String) ois.readObject();
 
-
-                String userName = sc.nextLine();
-                oos.writeObject(userName);
-                String returnValue = (String) ois.readObject();
-                System.out.println("El servidor responde: " + returnValue);
-                if (returnValue.equalsIgnoreCase("GoodBye")) {
-                    if (oos != null) oos.close();
-                    if (ois != null) ois.close();
-                    if (socket != null) socket.close();
-                    seguir = false;
-                    System.out.println("Conexión cerrada!");
+                            //COMPRUEBO SI) FINALIZO
+                            if (returnValue.equalsIgnoreCase("GoodBye")) {
+                                if (oos != null) oos.close();
+                                if (ois != null) ois.close();
+                                if (socket != null) socket.close();
+                                seguir = false;
+                                System.out.println("Conexión cerrada!");
+                            }else{
+                                //AÑADO AL ARRAY DE STRING EL MENSAJE INTRODUCIDO
+                                listMsg[listMsg.length-1]="<"+user+">" + " : [" + date +"]"+ "<"+returnValue+">";
+                                System.out.println("El servidor responde: " + returnValue);
+                                System.out.println("ALMACENO: "+listMsg[listMsg.length-1]);
+                            }
+                        }
+                    }
                 }
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
